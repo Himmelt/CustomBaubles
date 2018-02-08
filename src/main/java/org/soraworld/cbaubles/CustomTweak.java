f2dca29462b4be8d2a46653fe584eab206524981
Himmelt <master@void-3.cn>
2018-02-08 22:15:31 +0800

    mixins

--- src/main/java/org/soraworld/cbaubles/CustomTweak.java
+++ src/main/java/org/soraworld/cbaubles/CustomTweak.java
@@ -2,6 +2,8 @@ package org.soraworld.cbaubles;
 
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
 
 import java.io.File;
 import java.util.List;
@@ -10,6 +12,13 @@ public class CustomTweak implements ITweaker {
     @Override
     public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
         System.out.println("acceptOptions:" + gameDir + assetsDir + profile);
+        // This statement must appear first, failing to include
+        // it will cause a runtime error
+        MixinBootstrap.init();
+
+        // Retrieves the DEFAULT mixin environment and registers
+        // the config file
+        Mixins.addConfiguration("mixins.cbaubles.json");
     }
 
     @Override
