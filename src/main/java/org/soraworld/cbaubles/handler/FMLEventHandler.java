cf05822ab6093dd0da4820590c39c951891c11e8
Himmelt <master@void-3.cn>
2018-02-27 21:03:25 +0800

    AttributeModifier
    fix #1

--- src/main/java/org/soraworld/cbaubles/handler/FMLEventHandler.java
+++ src/main/java/org/soraworld/cbaubles/handler/FMLEventHandler.java
@@ -4,36 +4,27 @@ import baubles.api.BaublesApi;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import org.soraworld.cbaubles.items.Bauble;
 import org.soraworld.cbaubles.items.IItemStack;
+
+import java.util.UUID;
 
 public class FMLEventHandler {
 
     private byte ticks;
 
+    private static final UUID maxHealthUUID = UUID.fromString("da36b4ca-95ca-4614-9cb6-bc0c02bd9875");
+    private static final UUID moveSpeedUUID = UUID.fromString("05be2ef3-fd85-48d9-b1d0-0df3bb11f1c2");
+    private static final UUID attackDamageUUID = UUID.fromString("4652a225-f1f2-43ca-977f-aa6a8bb4a0cf");
+    private static final UUID knockResistUUID = UUID.fromString("76a2b773-3e97-48fc-a901-f8b9d464b685");
+
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if (ticks++ >= 20 && event.phase == TickEvent.Phase.START) {
-            double maxHealth = 20.0D;
-            double movementSpeed = 0.1D;
-            double attackDamage = 1.0D;
-            double knockbackResistance = 0.0D;
-
-            PotionEffect effect = event.player.getActivePotionEffect(Potion.field_76434_w);
-            if (effect != null) maxHealth += 4 * (effect.getAmplifier() + 1);
-            effect = event.player.getActivePotionEffect(Potion.moveSpeed);
-            if (effect != null) movementSpeed += 0.02 * (effect.getAmplifier() + 1);
-            effect = event.player.getActivePotionEffect(Potion.moveSlowdown);
-            if (effect != null) movementSpeed -= 0.015 * (effect.getAmplifier() + 1);
-            effect = event.player.getActivePotionEffect(Potion.damageBoost);
-            if (effect != null) attackDamage += 1.3 * (effect.getAmplifier() + 1);
-            effect = event.player.getActivePotionEffect(Potion.weakness);
-            if (effect != null) attackDamage -= 0.5 * (effect.getAmplifier() + 1);
-
+        if (ticks++ >= 25 && event.phase == TickEvent.Phase.START) {
+            double maxHealth = 0.0D, moveSpeed = 0.0D, attackDamage = 0.0D, knockResist = 0.0D;
             IInventory baubles = BaublesApi.getBaubles(event.player);
             for (int i = 0; baubles != null && i < baubles.getSizeInventory(); i++) {
                 ItemStack stack = baubles.getStackInSlot(i);
@@ -41,17 +32,36 @@ public class FMLEventHandler {
                     Bauble bauble = ((IItemStack) (Object) stack).getBauble();
                     if (bauble != null) {
                         maxHealth += bauble.getHP();
-                        movementSpeed += bauble.getSP();
+                        moveSpeed += bauble.getSP();
                         attackDamage += bauble.getAT();
-                        knockbackResistance += bauble.getKB() / 100.0D;
+                        knockResist += bauble.getKB() / 100.0D;
                     }
                 }
             }
 
-            event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(maxHealth);
-            event.player.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(movementSpeed);
-            event.player.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(attackDamage);
-            event.player.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(knockbackResistance);
+            AttributeModifier maxHealthModifier = new AttributeModifier(maxHealthUUID, "maxHealth", maxHealth, 0);
+            event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).removeModifier(maxHealthModifier);
+            if (maxHealth != 0) {
+                event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).applyModifier(maxHealthModifier);
+            }
+
+            AttributeModifier moveSpeedModifier = new AttributeModifier(moveSpeedUUID, "moveSpeed", moveSpeed, 0);
+            event.player.getEntityAttribute(SharedMonsterAttributes.movementSpeed).removeModifier(moveSpeedModifier);
+            if (moveSpeed != 0) {
+                event.player.getEntityAttribute(SharedMonsterAttributes.movementSpeed).applyModifier(moveSpeedModifier);
+            }
+
+            AttributeModifier attackDamageModifier = new AttributeModifier(attackDamageUUID, "attackDamage", attackDamage, 0);
+            event.player.getEntityAttribute(SharedMonsterAttributes.attackDamage).removeModifier(attackDamageModifier);
+            if (attackDamage != 0) {
+                event.player.getEntityAttribute(SharedMonsterAttributes.attackDamage).applyModifier(attackDamageModifier);
+            }
+
+            AttributeModifier knockResistModifier = new AttributeModifier(knockResistUUID, "knockResist", knockResist, 0);
+            event.player.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).removeModifier(knockResistModifier);
+            if (knockResist != 0) {
+                event.player.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).applyModifier(knockResistModifier);
+            }
 
             ticks = 0;
         }
