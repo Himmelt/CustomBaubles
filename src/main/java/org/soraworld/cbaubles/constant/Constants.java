c325b0368334ee2d0b78745308a0d006ed5af23f
Himmelt <master@void-3.cn>
2018-03-06 22:01:50 +0800

    1.0.1
    update lang

--- src/main/java/org/soraworld/cbaubles/constant/Constants.java
+++ src/main/java/org/soraworld/cbaubles/constant/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
 
     public static final String MOD_ID = "cbaubles";
     public static final String NAME = "CustomBaubles";
-    public static final String VERSION = "1.7.10-1.0.0";
+    public static final String VERSION = "1.7.10-1.0.1";
     public static final String AC_MC_VERSION = "[1.7.10]";
 
     public static final String ITEM_CUSTOM_BAUBLE = "custombauble";
